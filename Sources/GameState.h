24901432670ef32d389668643c1e9763279d9e56
Teemu Tuhkanen <t4tute00@students.oamk.fi>
2015-05-29 12:40:02 +0300

    statup

--- Sources/GameState.h
+++ Sources/GameState.h
@@ -2,3 +2,40 @@
  * GameState.h
  *
  */
+#ifndef __GAMESTATE_H_DEFINED__
+#define __GAMESTATE_H_DEFINED__
+
+#include "Window.h"
+#include "Input.h"
+#include "Font.h"
+#include "BaseState.h"
+#include "Text.h"
+#include "Color.h"
+#include "Texture.h"
+#include "Animation.h"
+#include "Level.h"
+
+class GameState : public BaseState
+{
+public:
+	GameState(Window *window);
+	~GameState();
+
+	stateStatus update();
+	void render();
+
+
+private:
+	Animation *test;
+	Window *window;
+	Level *level;
+	Texture *background;
+	Font *font;
+	Text *tooltip, signText;
+	std::string tooltip_s, signText_s;
+
+	SDL_Rect hilight;
+
+};
+
+#endif // __GAMESTATE_H_DEFINED__
--- Sources/MenuState.h
+++ Sources/MenuState.h
@@ -2,3 +2,41 @@
  * MenuState.h
  *
  */
+#ifndef __MENUSTATE_H_DEFINED__
+#define __MENUSTATE_H_DEFINED__
+#include "Window.h"
+#include "Input.h"
+#include "Font.h"
+#include "BaseState.h"
+#include "Text.h"
+#include "Color.h"
+#include "Texture.h"
+
+
+class MenuState : public BaseState 
+{
+
+public:
+	MenuState(Window *window);
+	~MenuState();
+
+	stateStatus update();
+	void render();
+
+
+private:
+	Texture selector;
+	int menu_index;
+
+	std::vector<Text *> menu_items;
+	Window *window;
+	Font *font;
+	int menu_x;
+	int menu_y;
+
+
+};
+
+
+
+#endif // __MENUSTATE_H_DEFINED__
