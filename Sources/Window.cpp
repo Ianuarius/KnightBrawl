4e6287b548134a36c7954f0c3e973ce153af4d3f
Teemu Tuhkanen <t4tute00@students.oamk.fi>
2015-06-02 11:21:57 +0300

    windou

--- Sources/Window.cpp
+++ Sources/Window.cpp
@@ -9,6 +9,7 @@ Window::Window(int window_width, int window_height):
 	window(nullptr),
 	renderer(nullptr)
 {
+
 	window = SDL_CreateWindow("KnightBrawl", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	
@@ -22,6 +23,59 @@ void Window::refresh()
 	SDL_RenderPresent(renderer);
 
 }
+
+void Window::destroy()
+{
+	if (this->renderer)
+	{
+		SDL_DestroyRenderer(this->renderer);
+		this->renderer =nullptr;
+	}
+
+	if (this->window)
+	{
+		SDL_DestroyWindow(this->window);
+		this->window = nullptr;
+	}
+}
+
+void Window::resize(int window_width, int window_height, bool fullscreen)
+{
+	destroy();
+
+	Uint32 window_flag;
+
+	this->fullscreen = fullscreen;
+
+	if (fullscreen)
+	{
+		window_flag = SDL_WINDOW_FULLSCREEN_DESKTOP;
+	}
+	else
+	{
+		window_flag = SDL_WINDOW_SHOWN;
+	}
+
+	/*if (!window || !renderer)
+	{
+		printf("Oops! \n");
+		printf( "SDL Error: %s \n", SDL_GetError());
+
+	}
+	*/
+
+	window = SDL_CreateWindow("KnightBrawl", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height, window_flag);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
+	SDL_RenderSetLogicalSize(renderer, logical_width, logical_height);
+	SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
+
+	this->window_width = window_width;
+	this->window_height = window_height;
+
+}
+
 
 SDL_Renderer *Window::getRenderer()
 {
--- Sources/Window.h
+++ Sources/Window.h
@@ -6,7 +6,6 @@
 #ifndef __WINDOW_H_DEFINED__
 #define __WINDOW_H_DEFINED__
 
-#include <string>
 #include "SDL.h"
 #include <stdio.h>
 #include "Input.h"
@@ -19,13 +18,23 @@ class Window
 {
 public:
 	Window(int window_width, int window_height);
-	void refresh();
+	
 	SDL_Renderer *getRenderer();
 	Uint32 getDelta();
-
+	
+	void destroy();
+	void resize(int window_width, int window_height, bool fullscreen);
+	void refresh();
 	/*void clear();
 	void fill(Color color);
 	*/
+
+	
+
+	unsigned int window_width, logical_width;
+	unsigned int window_height, logical_height;
+	unsigned int originalWidth;
+	unsigned int originalHeight;
 private:
 
 	SDL_Surface* gScreenSurface;
@@ -34,6 +43,8 @@ private:
 	SDL_Renderer *renderer;
 
 	Uint32 framerate, frame_delay, current_delta;
+
+	bool fullscreen;
 
 };
 
